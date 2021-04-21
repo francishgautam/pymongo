27bf182e086b5bf92e450053a0b17b73f1a8aed6
Francish Gautam <gautamfrancish@gmail.com>
2021-04-21 13:12:48 +0545

    FIRST COMMIT

---
+++ server.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['Mongo_URI'] = "mongodb+srv://dbUser:1234@cluster0.kihfc.mongodb.net/testDb?retryWrites=true&w=majority"
+
+########################################################################################
+
+if __name__ == "__main__":
+    app.run(port=80, debug=TRUE)
+
+#########################################################################################
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/create', methods=["GET", "POST"])
+def create():
+    if(request.method == 'GET'):
+        return render_template("create.html")
+    else:
+        movieName = request.form['movieName']
+        genre = request.form['genre']
+        director = request.form['director']
+
+        return redirect(url_for('index'))
+
+
+######################################################################################
+# import url_for
+# from flask_pymongo import pymongo
+
+# app = Flask(__name__)
+# app.config["MONGO_URI"] = ""
+# mongo = PyMongo(app)
+
+
+# @app.route("/")
+# def index():
+#     return '''
+#         <form method="POST" action="/create" enctype="multipart/form-data">
+#             <input type="text" name="movieName">
+#             <input type="text" name="genre">
+#             <input type="text" name="director">
+#             <input type="submit">
+#         </form>
+#         '''
